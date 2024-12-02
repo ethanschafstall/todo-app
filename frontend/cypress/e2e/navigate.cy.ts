70edc1529900184f5be9df8bbc3b14a54ac092e2
joaberch <122774888+joaberch@users.noreply.github.com>
2024-12-02 11:17:42 +0100

    chore(cypress navigation): add basis to test

---
+++ frontend/cypress/e2e/navigate.cy.ts
@@ -0,0 +1,97 @@
+describe('Check navigation', () => {
+    it('from login to login by logo', () => {
+    })
+    it('from login to about', () => {
+    })
+    it('from login to register', () => {
+    })
+    it('from login to dark theme login', () => {
+    })
+    it('from dark theme login to dark theme login by logo', () => {
+    })
+    it('from dark theme login to dark theme about', () => {
+    })
+    it('from dark theme login to dark theme register', () => {
+    })
+    it('from dark theme login to login', () => {
+    })
+    //TODO password forgotten
+    it('from register to login by logo', () => {
+    })
+    it('from register to login', () => {
+    })
+    it('from register to about', () => {
+    })
+    it('from register to dark theme register', () => {
+    })
+    it('from dark theme register to dark theme login by logo', () => {
+    })
+    it('from dark theme register to dark theme login', () => {
+    })
+    it('from dark theme register to dark theme about', () => {
+    })
+    it('from dark theme register to register', () => {
+    })
+    it('from about to login by logo', () => {
+    })
+    it('from about to login', () => {
+    })
+    it('from about to dark theme about', () => {
+    })
+    it('from dark theme about to dark theme login by logo', () => {
+    })
+    it('from dark theme about to dark theme login', () => {
+    })
+    it('from dark theme about to about', () => {
+    })
+    it('from route to route by logo', () => {
+    })
+    it('from route to route', () => {
+    })
+    it('from route to about', () => {
+    })
+    it('from route to profile', () => {
+    })
+    //TODO if diconnect light theme
+    it('from route to dark theme route', () => {
+    })
+    it('from dark theme route to dark theme route by logo', () => {
+    })
+    it('from dark theme route to dark theme route', () => {
+    })
+    it('from dark theme route to dark theme route', () => {
+    })
+    it('from dark theme route to dark theme about', () => {
+    })
+    it('from dark theme route to dark theme profile', () => {
+    })
+    //TODO if disconnect dark theme
+    it('from dark theme route to route', () => {
+    })
+    it('from profile to route by logo', () => {
+    })
+    it('from profile to route', () => {
+    })
+    it('from profile to about', () => {
+    })
+    it('from profile to profile', () => {
+    })
+    it('from profile to register', () => {
+    })
+    //TODO if disconnect light theme
+    it('from profile to dark theme profile', () => {
+    })
+    it('from dark theme profile to dark theme route by logo', () => {
+    })
+    it('from dark theme profile to dark theme route', () => {
+    })
+    it('from dark theme profile to dark theme about', () => {
+    })
+    it('from dark theme profile to dark theme profile', () => {
+    })
+    it('from dark theme profile to dark theme register', () => {
+    })
+    //TODO if disconnect dark theme
+    it('from dark theme profile to profile', () => {
+    })
+  })
