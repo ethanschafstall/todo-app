0fdc98f226315bf8c134340bdef99c3dbcfef6cc
joaberch <122774888+joaberch@users.noreply.github.com>
2024-12-02 11:23:08 +0100

    chore(cypress navigation): add the url to test

--- frontend/cypress/e2e/navigate.cy.ts
+++ frontend/cypress/e2e/navigate.cy.ts
@@ -1,97 +1,187 @@
 describe('Check navigation', () => {
     it('from login to login by logo', () => {
+      //Go to the /login page
+      cy.visit('/login')
     })
     it('from login to about', () => {
+      //Go to the /login page
+      cy.visit('/login')
     })
     it('from login to register', () => {
+      //Go to the /login page
+      cy.visit('/login')
     })
     it('from login to dark theme login', () => {
+      //Go to the /login page
+      cy.visit('/login')
     })
     it('from dark theme login to dark theme login by logo', () => {
+      //Go to the /login page
+      cy.visit('/login')
     })
     it('from dark theme login to dark theme about', () => {
+      //Go to the /login page
+      cy.visit('/login')
     })
     it('from dark theme login to dark theme register', () => {
+      //Go to the /login page
+      cy.visit('/login')
     })
     it('from dark theme login to login', () => {
+      //Go to the /login page
+      cy.visit('/login')
     })
     //TODO password forgotten
     it('from register to login by logo', () => {
+      //Go to the /register page
+      cy.visit('/register')
     })
     it('from register to login', () => {
+      //Go to the /register page
+      cy.visit('/register')
     })
     it('from register to about', () => {
+      //Go to the /register page
+      cy.visit('/register')
     })
     it('from register to dark theme register', () => {
+      //Go to the /register page
+      cy.visit('/register')
     })
     it('from dark theme register to dark theme login by logo', () => {
+      //Go to the /register page
+      cy.visit('/register')
     })
     it('from dark theme register to dark theme login', () => {
+      //Go to the /register page
+      cy.visit('/register')
     })
     it('from dark theme register to dark theme about', () => {
+      //Go to the /register page
+      cy.visit('/register')
     })
     it('from dark theme register to register', () => {
+      //Go to the /register page
+      cy.visit('/register')
     })
     it('from about to login by logo', () => {
+      //Go to the /about page
+      cy.visit('/about')
     })
     it('from about to login', () => {
+      //Go to the /about page
+      cy.visit('/about')
     })
     it('from about to dark theme about', () => {
+      //Go to the /about page
+      cy.visit('/about')
     })
     it('from dark theme about to dark theme login by logo', () => {
+      //Go to the /about page
+      cy.visit('/about')
     })
     it('from dark theme about to dark theme login', () => {
+      //Go to the /about page
+      cy.visit('/about')
     })
     it('from dark theme about to about', () => {
+      //Go to the /about page
+      cy.visit('/about')
     })
     it('from route to route by logo', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from route to route', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from route to about', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from route to profile', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     //TODO if diconnect light theme
     it('from route to dark theme route', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from dark theme route to dark theme route by logo', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from dark theme route to dark theme route', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from dark theme route to dark theme route', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from dark theme route to dark theme about', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from dark theme route to dark theme profile', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     //TODO if disconnect dark theme
     it('from dark theme route to route', () => {
+      //Go to the / page
+      cy.visit('/')
     })
     it('from profile to route by logo', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from profile to route', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from profile to about', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from profile to profile', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from profile to register', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     //TODO if disconnect light theme
     it('from profile to dark theme profile', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from dark theme profile to dark theme route by logo', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from dark theme profile to dark theme route', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from dark theme profile to dark theme about', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from dark theme profile to dark theme profile', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     it('from dark theme profile to dark theme register', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
     //TODO if disconnect dark theme
     it('from dark theme profile to profile', () => {
+      //Go to the /profile page
+      cy.visit('/profile')
     })
   })
