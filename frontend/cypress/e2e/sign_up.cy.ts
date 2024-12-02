9b69502d65275be87b14e46c50914141849ca70c
joaberch <122774888+joaberch@users.noreply.github.com>
2024-12-02 10:31:05 +0100

    feat(cypress sign_up): check invalid input

--- frontend/cypress/e2e/sign_up.cy.ts
+++ frontend/cypress/e2e/sign_up.cy.ts
@@ -21,6 +21,40 @@ describe('Create an account', () => {
     cy.visit('/register')
 
     //Check the error message has appeared 3 times
-    cy.contains('Vous devez renseigner ce champ').should('have.length', 3)
+    cy.contains('Vous devez renseigner ce champ').should('be.visible') //TODO check if it appears 3 times
+  })
+  it('invalid email', () => {
+    //Go to the /register page
+    cy.visit('/register')
+
+    //type in input
+    cy.get('input[name="email"]').type('InvalidEmail')
+
+    //Click the create an account button
+    cy.contains('Créer un compte').click()
+
+    //Check the error message has appeared
+    cy.contains('Format email incorrect').should('be.visible')
+  })
+  it('invalid password', () => {
+    //Go to the /register page
+    cy.visit('/register')
+
+    //type in input
+    cy.get('input[name="password"]').type('1')
+
+    //Check the error message has appeared
+    cy.contains('Le mot de passe doit faire au moins 5 caractères').should('be.visible')
+  })
+  it('password not matching', () => {
+    //Go to the /register page
+    cy.visit('/register')
+
+    //type in input
+    cy.get('input[name="password"]').type('password1')
+    cy.get('input[name="confirmation"]').type('password2')
+
+    //Check the error message has appeared
+    cy.contains('Les mots de passe ne correspondent pas').should('be.visible')
   })
 })
