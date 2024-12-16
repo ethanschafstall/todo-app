f76ab9e656abd051a6a2c764b8711cf5c8640d14
joaberch <122774888+joaberch@users.noreply.github.com>
2024-12-16 10:49:03 +0100

    fix(login/logout): use defined string

--- frontend/cypress/e2e/login_logout.cy.ts
+++ frontend/cypress/e2e/login_logout.cy.ts
@@ -2,7 +2,7 @@ const DISCONNECT_BUTTON = '#headlessui-menu-item-v-4'
 
 describe('Check', () => {
     it('creating an account', () => {
-        cy.visit(REGISTER_URL)
+        cy.visit('/register')
         cy.get('input[name="email"]').type('testuser2@example.com')
         cy.get('input[name="password"]').type('1!TestPassword...')
         cy.get('input[name="confirmation"]').type('1!TestPassword...')
@@ -10,14 +10,14 @@ describe('Check', () => {
         cy.url().should('contain', LOGIN_URL) //We consider that redirection to the login page is an account created
     })
     it('login into the account', () => {
-        cy.visit(LOGIN_URL)
+        cy.visit('/login')
         cy.get('input[name="email"]').type('testuser@example.com')
         cy.get('input[name="password"]').type('1!TestPassword...')
         cy.contains(CONNECT_BUTTON_TEXT).click()
         cy.url().should('equal', ROUTE_URL) //In that case the test to create the account worked
     })
     it('disconnecting', () => {
-        cy.visit(ROUTE_URL)
+        cy.visit('/')
         cy.get(LOGGED_BUTTON).click()
         cy.get(DISCONNECT_BUTTON).click()
         cy.url().should('contain', LOGIN_URL) //Should redirect to the login menu
